bab662511e8202842b1307cfade6ec7698484d0c
Jona615m <joole24@student.sdu.dk>
2024-11-23 17:35:32 +0100

    Useless classes deleted

--- obj/Debug/net8.0/temp.AssemblyInfo.cs
+++ obj/Debug/net8.0/temp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("temp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ff31ad13160e8629569c37364b453fbf479ebab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5835246ca2b29f20dde5dbed8099d1355ae71dda")]
 [assembly: System.Reflection.AssemblyProductAttribute("temp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("temp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
