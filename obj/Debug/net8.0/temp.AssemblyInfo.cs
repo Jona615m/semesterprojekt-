e0ab11715acb70b7ae4022c4acc3f8dd6d073203
Jona615m <joole24@student.sdu.dk>
2024-12-03 11:44:42 +0100

    Game test 2

--- obj/Debug/net8.0/temp.AssemblyInfo.cs
+++ obj/Debug/net8.0/temp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("temp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0f1f70d2e04f90ee83eb61cde9a128d4582b498")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfa185dac97dd42eb91ce514eb85ee1bf1adc1ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("temp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("temp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
